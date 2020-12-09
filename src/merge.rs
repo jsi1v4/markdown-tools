df3deb1a9add4610f95d2ed17b052780c5127884
Jose Silva <me@jsi1v4.dev>
2020-12-09 01:35:04 -0300

    merge to param must be file

--- src/merge.rs
+++ src/merge.rs
@@ -5,6 +5,10 @@ pub fn run() {
   let from = env::args().nth(2).expect("First arg 'from' is required!");
   let to = env::args().nth(3).expect("Second arg 'to' is required!");
 
+  if utils::is_dir(&to) {
+    panic!("Second arg 'to' must be file!");
+  }
+
   let mut content = String::new();
   if utils::is_dir(&from) {
     let mut paths = utils::read_dir(&from);
